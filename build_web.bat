67e89f621e7db9311018a50a688d09d1fc18000f
Vincent Wang <376787823@qq.com>
2026-09-04 20:35:16 +0800

    Bump Windows static web build version to 2.1.0

--- build_web.bat
+++ build_web.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 set "OUT=dist\web"
 
 echo ============================================================
-echo   HelloLabel 1.5 - Build Static Web Distribution
+echo   HelloLabel 2.1 - Build Static Web Distribution
 echo ============================================================
 
 if exist "%OUT%" rmdir /s /q "%OUT%"
@@ -17,7 +17,7 @@ copy /y "static\index.html" "%OUT%\index.html" >nul
 if errorlevel 1 goto :error
 if exist "%OUT%\static\index.html" del /q "%OUT%\static\index.html"
 
-> "%OUT%\VERSION.txt" echo HelloLabel 1.5.0 - browser-only static runtime
+> "%OUT%\VERSION.txt" echo HelloLabel 2.1.0 - browser-only static runtime
 
 echo [OK] Static site created at %OUT%
 echo      Upload the CONTENTS of %OUT% to your Nginx document root.
