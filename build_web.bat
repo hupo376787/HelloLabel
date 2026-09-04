af4d144bad5c2a542c7b10a39bf3b5b7caf16a11
Vincent Wang <376787823@qq.com>
2026-09-04 23:41:20 +0800

    Add Cloudflare Pages headers to web build

--- build_web.bat
+++ build_web.bat
@@ -15,12 +15,14 @@ xcopy /e /i /y "static\*" "%OUT%\static\" >nul
 if errorlevel 1 goto :error
 copy /y "static\index.html" "%OUT%\index.html" >nul
 if errorlevel 1 goto :error
+copy /y "_headers" "%OUT%\_headers" >nul
+if errorlevel 1 goto :error
 if exist "%OUT%\static\index.html" del /q "%OUT%\static\index.html"
 
 > "%OUT%\VERSION.txt" echo HelloLabel 2.1.0 - browser-only static runtime
 
 echo [OK] Static site created at %OUT%
-echo      Upload the CONTENTS of %OUT% to your Nginx document root.
+echo      Upload the CONTENTS of %OUT% to your Nginx document root or Cloudflare Pages.
 echo      No Python, FastAPI, Uvicorn, OpenCV or server-side AI is required.
 exit /b 0
 
