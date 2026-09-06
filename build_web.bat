c664ce1662fe55a2b131aef0ce1ed08142500836
Vincent Wang <376787823@qq.com>
2026-09-06 22:05:43 +0800

    Bump Windows web build metadata to 2.2.0

--- build_web.bat
+++ build_web.bat
@@ -3,10 +3,10 @@ setlocal EnableExtensions
 cd /d "%~dp0"
 
 set "OUT=dist\web"
-set "CACHE_TOKEN=hellolabel-v210-t7"
+set "CACHE_TOKEN=hellolabel-v220-t1"
 
 echo ============================================================
-echo   HelloLabel 2.1 - Build Static Web Distribution
+echo   HelloLabel 2.2 - Build Static Web Distribution
 echo ============================================================
 
 if exist "%OUT%" rmdir /s /q "%OUT%"
@@ -27,7 +27,7 @@ copy /y "_redirects" "%OUT%\_redirects" >nul
 if errorlevel 1 goto :error
 if exist "%OUT%\static\index.html" del /q "%OUT%\static\index.html"
 
-> "%OUT%\VERSION.txt" echo HelloLabel 2.1.0 - browser-only static runtime
+> "%OUT%\VERSION.txt" echo HelloLabel 2.2.0 - browser-only static runtime
 
 echo [OK] Static site created at %OUT%
 echo      Upload the CONTENTS of %OUT% to your Nginx document root or Cloudflare Pages.
